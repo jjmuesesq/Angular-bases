6836318d43cdb91f5eceb10c2761614c152b4521
jjmuesesq <jjmuesesq@unal.edu.co>
2024-06-10 11:49:25 -0500

    :sparkles: Servicios

--- src/app/dbz/pages/main-page-component.ts
+++ src/app/dbz/pages/main-page-component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
     selector: 'app-dbz-main-page', // indica que es un componente personalizado
@@ -7,26 +8,7 @@ import { Character } from '../interfaces/character.interface';
 })
 
 export class MainPageComponent {
-    public characters: Character[] = [
-        {
-            name: 'Krillin',
-            power: 1000
-        },
-        {
-            name: 'Goku',
-            power: 9500
-        },
-        {
-            name: 'Vegeta',
-            power: 7500
-        }
-    ];
-
-    onNewCharacter( character: Character):void {
-        this.characters.push(character);
-    }
-
-    onDeleteCharacter(index:number){
-        this.characters.splice(index, 1) //emision, recibirlo, suscribirse y realizar algo con la emision del evento
+    constructor ( public dbzService: DbzService){ 
+        //inyección de dependencias, me habilta en mi componente page toda la informacion utilizada en el servicio
     }
 }
